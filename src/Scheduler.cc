53b4c687553816a149a9d97d5105822683966a50
Xiao Luo <luo448@purdue.edu>
2024-11-12 16:46:59 -0500

    reduce wait timer elapse

--- src/Scheduler.cc
+++ src/Scheduler.cc
@@ -673,7 +673,8 @@ Scheduler::Rescheduled Scheduler::reschedule(Switchable switchable) {
           #if XDEBUG_LATENCY
           auto start_time = chrono::steady_clock::now();
           #endif
-          current_->wait(timeout);
+          //current_->wait(timeout);
+          current_->wait(0.001);
           #if XDEBUG_LATENCY
             stopped_after_wait = true;
             after_wait = chrono::steady_clock::now();
