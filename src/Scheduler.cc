862865e74be7d7f3e2ae55b2d9afcf4347a01030
Xiao Luo <luo448@purdue.edu>
2024-11-13 16:48:08 -0500

    remove interception library scratch buffer

--- src/Scheduler.cc
+++ src/Scheduler.cc
@@ -673,9 +673,7 @@ Scheduler::Rescheduled Scheduler::reschedule(Switchable switchable) {
           #if XDEBUG_LATENCY
           auto start_time = chrono::steady_clock::now();
           #endif
-          //current_->wait(timeout);
-          //LOG(debug) << "timer timeout value: " << timeout;
-          current_->wait(0.000001);
+          current_->wait(timeout);
           #if XDEBUG_LATENCY
             stopped_after_wait = true;
             after_wait = chrono::steady_clock::now();
