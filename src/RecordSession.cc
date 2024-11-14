01f967f02e078be3f9503f48d8a84092970ad62b
Xiao Luo <luo448@purdue.edu>
2024-11-14 16:42:46 -0500

    explore

--- src/RecordSession.cc
+++ src/RecordSession.cc
@@ -2610,8 +2610,6 @@ RecordSession::RecordResult RecordSession::record_step() {
   }
 #endif
 
-  return result;
-
   // LOG(debug) << "[workflow] scheduling: " << curr_sched_time << " ms";
   if (rescheduled.interrupted_by_signal) {
     // The scheduler was waiting for some task to become active, but was
