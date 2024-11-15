15d22ab316a2d219feac7355cc8fbd4c631fb984
Xiao Luo <luo448@purdue.edu>
2024-11-15 13:49:18 -0500

    fix

--- src/RecordSession.cc
+++ src/RecordSession.cc
@@ -2820,7 +2820,6 @@ void RecordSession::term_detached_tasks() {
 }
 
 void RecordSession::close_trace_writer(TraceWriter::CloseStatus status) {
-  return;
   trace_out.close(status, trace_id.get());
 }
 
